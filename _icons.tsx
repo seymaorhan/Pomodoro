df7f0ec25f6430356396cd4cb5090e4407b9fc78
seymaorhann <120644957+seymaorhan@users.noreply.github.com>
2025-12-09 21:00:27 +0300

    |icons.tsx yükledim

--- _icons.tsx
+++ _icons.tsx
@@ -1,30 +1,23 @@
-// app/_icons.ts
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
 
-type IconProps = { size: number; color: string };
-
-export const categoryIcons: Record<
-  string,
-  React.FC<IconProps>
-> = {
-  Kodlama: ({ size, color }) => (
+export const categoryIcons: any = {
+  Kodlama: (size: number, color: string) => (
     <MaterialCommunityIcons name="code-tags" size={size} color={color} />
   ),
-
-  Ders: ({ size, color }) => (
+  Ders: (size: number, color: string) => (
     <MaterialCommunityIcons name="school" size={size} color={color} />
   ),
-
-  Proje: ({ size, color }) => (
+  Proje: (size: number, color: string) => (
     <MaterialCommunityIcons name="briefcase" size={size} color={color} />
   ),
-
-  Kitap: ({ size, color }) => (
-    <MaterialCommunityIcons
-      name="book-open-variant"
-      size={size}
-      color={color}
-    />
+  Kitap: (size: number, color: string) => (
+    <MaterialCommunityIcons name="book-open-variant" size={size} color={color} />
+  ),
+  Okuma: (size: number, color: string) => (
+    <MaterialCommunityIcons name="book" size={size} color={color} />
+  ),
+  Araştırma: (size: number, color: string) => (
+    <MaterialCommunityIcons name="magnify" size={size} color={color} />
   ),
 };
